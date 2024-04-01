a25b74ede6e307be68b5d89c5f9c6cc3b15b7120
agusKLY <79623381+agusKLY@users.noreply.github.com>
2024-04-01 14:29:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ function generateCookie() {
         for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
         return result;
     }
+    function generateUUID(){
+        var d = (new Date).getTime();
+        var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == "x" ? r : r & 3 | 8).toString(16)
+        });
+        return uuid
+    }
 
     var cookieName = "__Host-githubchips";
     var cookieValue = "";
@@ -22,7 +31,8 @@ function generateCookie() {
     // Check if cookie already exists
     if (!cookieValue) {
         // Generate a new cookie value if it doesn't exist
-        cookieValue = randomString(12);
+        //cookieValue = randomString(12);
+        cookieValue = generateUUID();
     }
 
     // Create a cookie with the generated or existing value (set path to ensure accessibility from domainB.com)
