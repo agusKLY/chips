cd1507da5c79d7373f294b80f3a52e3a705d4eb8
agusKLY <79623381+agusKLY@users.noreply.github.com>
2024-04-02 13:44:14 +0700

    Update index-chips.js

--- index-chips.js
+++ index-chips.js
@@ -1,15 +1,15 @@
-.function generateCookie() {
-    // Function to generate a random alphanumeric string as UUID
-    function generateUUID(){
-        var d = (new Date).getTime();
-        var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-            var r = (d + Math.random() * 16) % 16 | 0;
-            d = Math.floor(d / 16);
-            return (c == "x" ? r : r & 3 | 8).toString(16)
-        });
-        return uuid
-    }
-
+window.kly = {};
+// Function to generate a random alphanumeric string as UUID
+function generateUUID(){
+    var d = (new Date).getTime();
+    var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == "x" ? r : r & 3 | 8).toString(16)
+    });
+    return uuid
+}
+function generateCookie() {
     var cookieName = "__Host-githubchips";
     var cookieValue = "";
     var cookies = document.cookie.split(';');
@@ -25,12 +25,18 @@
     // Check if cookie already exists
     if (!cookieValue) {
         // Generate a new cookie value if it doesn't exist
-        //cookieValue = randomString(12);
         cookieValue = generateUUID();
     }
 
     // Create a cookie with the generated or existing value (set path to ensure accessibility from parent domain)
-    document.cookie = cookieName + "=" + cookieValue + "; Secure; Path=/; SameSite=None; Partitioned;"
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + "; Secure;" + expires + "; Path=/; SameSite=None; Partitioned;"
+
+    // Create custom variable to indicate the cookies already created
+    // This var will be used on another code as checker to avoid race time condition
+    window.kly.UUIDexist = true;
 
     console.log("Cookies created using code on github domain")
 }
